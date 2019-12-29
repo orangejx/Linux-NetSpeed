b4f80451504451a6c8949a9aeaecccedd01494c0
root <root@california-us.ora.pub>
2019-12-29 23:43:44 +0800

    update tcp.sh.

--- tcp.sh
+++ tcp.sh
@@ -7,11 +7,13 @@ export PATH
 #	Description: BBR+BBR魔改版+BBRplus+Lotserver
 #	Version: 1.3.1
 #	Author: 千影,cx9208
-#	Blog: https://www.94ish.me/
+#	Author Blog: https://www.94ish.me/
+#       Forkeder: orangejx[ora.pub]
+#	Forkeder Blog: https://ora.pub
 #=================================================
 
 sh_ver="1.3.1"
-github="raw.githubusercontent.com/cx9208/Linux-NetSpeed/master"
+github="raw.githubusercontent.com/orangejx/Linux-NetSpeed/master"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
